d32cfe69b466235252458c3c1735a0f4094f8964
givensuman <givensuman@gmail.com>
2025-08-21 12:26:37 -0400

    Continue writing install, not working completely

--- functions/install.zsh
+++ functions/install.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
 function plugin-install {
+  # Define git binary path, necessary due to
+  # PATH issues with nested functions
+  git_bin=$(which git)
+
   # TODO: Improve usage message
   if [[ $# == 0 || $1 =~ "--help" || $1 =~ "-h" ]]; then
     echo "Usage: plugin install [name...]"
@@ -12,8 +16,10 @@ function plugin-install {
     local plugin_arg=$1
     local path=$2
     local branch=$3
+    # Necessary for PATH issues with 
+    # nested functions
 
-    # Filled in later
+    # Fill in variables
     local git_url=""
     local repo_name=""
 
@@ -32,7 +38,63 @@ function plugin-install {
       echo "${fg[yellow]}Invalid plugin name \"$plugin_arg\"${reset_color}"
     fi
 
-    # TODO: Continue implementation
+    echo "repo_name: $repo_name"
+    echo "git_url: $git_url"
+    echo ""
+    echo "plugin_arg: $plugin_arg"
+    echo "path: $path"
+    echo "branch: $branch"
+
+
+    # Handle cases where directory exists
+    if [[ -d "$ZPLUGINDIR/$repo_name" ]]; then
+      pushd "$ZPLUGINDIR/$repo_name" > /dev/null
+
+      if [[ $path != "" ]]; then
+        if [[ "$(git config --get core.sparseCheckout)" == "true" ]]; then
+          ($git_bin sparse-checkout add "$path") > /dev/null
+
+          popd > /dev/null
+          # TODO: Error message
+          return $?
+        else
+          echo "${fg[yellow]}Plugin parent \"$repo_name\" exists and is is not a sparse-checkout${reset_color}"
+
+          popd > /dev/null
+          return 1
+        fi
+      else # May be sparse-checkout
+        if [[ "$(git config --get core.sparseCheckout)" == "true" ]]; then
+          ($git_bin sparse-checkout disable) > /dev/null
+
+          popd > /dev/null
+          # TODO: Error message
+          return $?
+        else # Not a sparse-checkout, directory exists, path not specified
+          echo "${fg[yellow]}Plugin \"$repo_name\" already installed${reset_color}"
+
+          popd > /dev/null
+          return 1
+        fi
+      fi
+    fi
+
+    # Install the plugin
+    if [[ $branch != "" ]]; then
+      ($git_bin clone -b "$branch" "$git_url" "$ZPLUGINDIR/$repo_name") > /dev/null
+      # TODO: Error message
+    else
+      ($git_bin clone "$git_url" "$ZPLUGINDIR/$repo_name") > /dev/null
+      # TODO: Error message
+    fi
+
+    if [[ $path != "" ]]; then
+      pushd "$ZPLUGINDIR/$repo_name" > /dev/null
+
+      git sparse-checkout init --cone > /dev/null
+      git sparse-checkout add "$path" > /dev/null
+      # TODO: Error message
+    fi
   }
 
   local path=""
@@ -71,7 +133,7 @@ function plugin-install {
     if [[ "$path" != "" || "$branch" != "" ]]; then
       echo "${fg[yellow]}Cannot specify path and branch for multiple plugins${reset_color}"
     else
-      for plugin in $positional_args; do
+      for plugin in "$positional_args[@]"; do
         install-plugin-by-arg "$plugin" "" ""
       done
     fi
